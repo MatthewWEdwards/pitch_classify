0f7e4c22361f80ae7f8be06c8e84237c3221c667
MatthewWEdwards <mathew41796@gmail.com>
2018-12-26 23:20:11 -0500

    Refactor multiwidget to have gboxes

--- src/multi_widget.py
+++ src/multi_widget.py
@@ -8,33 +8,52 @@ from note_widget import NoteWidget
 from system_widget import SystemWidget
 from audio_singleton import AudioSingleton
 
-class MultiWidget(QWidget):
+class MultiWidget(QtWidgets.QMainWindow):
 
     exist = True
     
     def __init__(self, spec = None, pitch = None, note = None, sys_w = None):
-        super(QWidget, self).__init__()
-        self.layout = QtGui.QGridLayout()
+        super().__init__()
+        self.setObjectName("MainWindow")
+        self.setWindowTitle("Pitch Classify")
+        
+        self.w = QtWidgets.QWidget()
+        self.setCentralWidget(self.w)
+        self.layout = QtGui.QGridLayout(self.w)
         self.setLayout(self.layout)
 
         self.audio_singleton = AudioSingleton()
             
         if spec is None:
             spec = SpectrumWidget()
+        self.spec_gbox = QtWidgets.QGroupBox("Spectrum")
+        self.spec_gbox_l = QtWidgets.QGridLayout(self.spec_gbox)
+        self.spec_gbox_l.addWidget(spec)
+        self.layout.addWidget(self.spec_gbox, 0, 1, 8, 3)
+
         if note is None:
             note = NoteWidget()
+        self.note_gbox = QtWidgets.QGroupBox("Note Plot")
+        self.note_gbox_l = QtWidgets.QGridLayout(self.note_gbox)
+        self.note_gbox_l.addWidget(note)
+        self.layout.addWidget(self.note_gbox, 0, 4, 4, 2)
+
         if pitch is None:
             pitch = PitchWidget(note_widget = note)
+        self.pitch_gbox = QtWidgets.QGroupBox("Pitch Plot")
+        self.pitch_gbox_l = QtWidgets.QGridLayout(self.pitch_gbox)
+        self.pitch_gbox_l.addWidget(pitch)
+        self.layout.addWidget(self.pitch_gbox, 4, 4, 4, 1)
+
         if sys_w is None:
             sys_w = SystemWidget([spec, pitch], quit=False)
+        self.sys_gbox = QtWidgets.QGroupBox("System Control")
+        self.sys_gbox_l = QtWidgets.QGridLayout(self.sys_gbox)
+        self.sys_gbox_l.addWidget(sys_w)
+        self.layout.addWidget(self.sys_gbox, 0, 0)
 
-        self.widgets = [sys_w, spec, pitch, note]
-
-        for widget in range(len(self.widgets)):
-            self.layout.addWidget(self.widgets[widget], widget, 0)
-        
         self.quit_btn = QPushButton('Quit')
-        self.layout.addWidget(self.quit_btn, 4, 0)
+        self.layout.addWidget(self.quit_btn, 1, 0)
         self.quit_btn.clicked.connect(self.quit)
 
     def quit(self):
@@ -43,18 +62,13 @@ class MultiWidget(QWidget):
         self.exist = False
         
 if __name__ == '__main__':
-    # Needed because pyQT remains in spyder namespace
-    app = QtCore.QCoreApplication.instance()
-    if app is None:
-        app = QtWidgets.QApplication(sys.argv)
-    
-    m_w = MultiWidget()
-    m_w.show()
-    while m_w.exist:
-        QtGui.QApplication.processEvents()
-    m_w.close()
-    logscale = False
-    decibles = False
-    hann = True
-    playing_flag = False
-        
+    myApp = QtWidgets.QApplication.instance()
+    if myApp is None:
+        args = list(sys.argv)
+        #args[1:1] = ['-stylesheet', stylesheet_fname]
+        myApp = QtWidgets.QApplication(args)
+    myWin = MultiWidget()
+    myWin.show()
+    if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+        QtWidgets.QApplication.instance().exec_()
+
